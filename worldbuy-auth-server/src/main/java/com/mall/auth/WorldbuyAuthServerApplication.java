9b90110275438149355a1db0ce93f5b7e994e3f7
lixinjian <963597661@qq.com>
2022-08-07 03:17:05 +0100

    add auth moudule

---
+++ worldbuy-auth-server/src/main/java/com/mall/auth/WorldbuyAuthServerApplication.java
@@ -0,0 +1,17 @@
+package com.mall.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+public class WorldbuyAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldbuyAuthServerApplication.class, args);
+    }
+
+}
